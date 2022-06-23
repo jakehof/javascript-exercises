59ca996913ca26901b9cb6ee15fd79065ab395ed
Jake Hofmann <Jfshofmann@gmail.com>
2022-06-23 15:08:11 -0400

    Test reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    let letters = Array.from(string);
+    letters.reverse();
+    let backwordsString = letters.join('');
+    return backwordsString;
 };
 
 // Do not edit below this line
