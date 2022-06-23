4eb4b553307506da53689b0782c71f44453df53f
Jake Hofmann <Jfshofmann@gmail.com>
2022-06-23 15:02:45 -0400

    Test repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let value = '';
+    let i = 0;
+    if (num < 0) {
+        return 'ERROR';
+    }
+    while (i < num) {
+        value += (string);
+        i++;
+    }
+    return value;
+    
 };
 
 // Do not edit below this line
