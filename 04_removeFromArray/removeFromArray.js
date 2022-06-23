3bb291e0cab76a7aa68f3b781ce76dae647803a5
Jake Hofmann <Jfshofmann@gmail.com>
2022-06-23 16:41:07 -0400

    test removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+//Accept array as arg 1, create array from additional args
 
+const removeFromArray = function(array, ...b) {
+    console.log(array);
+    //iterate through array provided
+    for (let i = array.length; i >= 0; i--){
+        //for each item in array provided check against each item of b
+        for(let j = 0; j < b.length; j++) {
+            //if the items match, remove the item from the original array
+            
+            if (array[i] === b[j]){
+                array.splice(i, 1);
+                
+            }
+        } 
+        
+    }
+    return array;
+        
+    
 };
 
 // Do not edit below this line
