557eb89f5973183f6fbedc78843604adaf9a2f43
Jake Hofmann <Jfshofmann@gmail.com>
2022-06-30 18:42:24 -0400

    Add tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let conv = Math.round((temp - 32) * (5/9) * 10) / 10;
+  return conv;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let conv = Math.round((temp * (9/5) + 32) * 10) / 10;
+  return conv;
 };
 
 // Do not edit below this line
