86562e5ddd6fdb17da1a695e5e9c1138795baca2
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-24 11:16:54 -0400

    Finish functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let rslt = 0;
+  for(i=0; i < arr.length; i++) {
+    rslt += arr[i];
+  }
+  return rslt;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let rslt = 1;
+  for(i=0; i < arr.length; i++){
+    console.log(arr[i]);
+    rslt *=  arr[i];
+  }
+  return rslt;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let rslt=1, i;
+  if(a === 0 || a === 1){
+    return 1;
+  } 
+  for(i=2;i <= a; i++){
+    rslt *= i;
+  }
+  return rslt;
 };
 
 // Do not edit below this line
