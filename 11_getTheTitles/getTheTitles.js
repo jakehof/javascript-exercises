4253701cc877f4adc16ff89e4177b586ff903bc5
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-25 22:46:15 -0400

    Add get titles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let newArr = [];
+    books.forEach(element => {
+        console.log(element);
+        console.log(element.title);
+        newArr.push(element.title);
+    });
+    return newArr;
 };
 
 // Do not edit below this line
