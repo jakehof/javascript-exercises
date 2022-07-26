c6d87d861e5c5181e4f79f2a184f95b83597bdeb
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-25 20:03:00 -0400

    Add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    let punctuationless = palindrome.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase().replace(/\s/g, '');
+    console.log(punctuationless);
+    let arr = Array.from(punctuationless);
+    let reversed = arr.reverse();
+    let newString = reversed.join("");
+    if(newString === punctuationless){
+        return true;
+    } else {return false};
 };
 
 // Do not edit below this line
