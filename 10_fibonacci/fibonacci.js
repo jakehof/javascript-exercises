94304df8a08ec2316f26c3c4dc2a7dfcb8130394
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-25 20:52:59 -0400

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if(position>=0){
+        let newArray = Array(position);
+        newArray[0] = 0;
+        newArray[1] = 1;
 
+        for (let i = 2; i <= position; i++){
+            newArray[i] = newArray[i - 1] + newArray[i - 2];
+        }
+        return newArray[position];
+    } else {return "OOPS"}
 };
 
 // Do not edit below this line
