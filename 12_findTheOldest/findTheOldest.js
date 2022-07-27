f4c27ee92910cc880ec9f12bdbc58f41d6e4a6e1
Jake Hofmann <Jfshofmann@gmail.com>
2022-07-26 20:57:04 -0400

    add findtheoldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort(function(a, b) {
+        const firstAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const secondAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        if(firstAge > secondAge){
+            return -1;
+        } else {return 1;}    
+    })
+    return people[0];
+};
 
+function getAge(birth, death){
+    if (!death) {
+        death = new Date().getFullYear(); // return current year using Date()
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
